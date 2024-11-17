70087922e6a8795fb2f6ffc7e936f4e023a72571
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-17 16:01:58 +0800

    fix cmake

--- src/robot_control/setup.py
+++ src/robot_control/setup.py
@@ -7,15 +7,12 @@ package_name = 'robot_control'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name, f'{package_name}.nodes'],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'msg'), glob('msg/*.msg')),
-        (os.path.join('share', package_name, 'srv'), glob('srv/*.srv')),
-        (os.path.join('share', package_name, 'action'), glob('action/*.action'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
